5c288944b34808198fc9a9a4592ee5314599c477
kostya <modestovkostya.gmail.com>
2015-03-21 21:43:22 +0300

    проект

---
+++ rozego/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace rozego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Back = new System.Windows.Forms.ToolStripButton();
+            this.forward = new System.Windows.Forms.ToolStripButton();
+            this.refresh = new System.Windows.Forms.ToolStripButton();
+            this.stop = new System.Windows.Forms.ToolStripButton();
+            this.home = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.start = new System.Windows.Forms.ToolStripButton();
+            this.adress = new System.Windows.Forms.ToolStripComboBox();
+            this.zakladki = new System.Windows.Forms.ToolStripButton();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.основноеМенюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.назадToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закладкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Back,
+            this.forward,
+            this.refresh,
+            this.stop,
+            this.home,
+            this.toolStripSeparator1,
+            this.start,
+            this.adress,
+            this.zakladki});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1016, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Back
+            // 
+            this.Back.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Back.Image = ((System.Drawing.Image)(resources.GetObject("Back.Image")));
+            this.Back.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(43, 22);
+            this.Back.Text = "Назад";
+            this.Back.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // forward
+            // 
+            this.forward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.forward.Image = ((System.Drawing.Image)(resources.GetObject("forward.Image")));
+            this.forward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(50, 22);
+            this.forward.Text = "Вперед";
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(65, 22);
+            this.refresh.Text = "Обновить";
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // stop
+            // 
+            this.stop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.stop.Image = ((System.Drawing.Image)(resources.GetObject("stop.Image")));
+            this.stop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(38, 22);
+            this.stop.Text = "Стоп";
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // home
+            // 
+            this.home.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.home.Image = ((System.Drawing.Image)(resources.GetObject("home.Image")));
+            this.home.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.home.Name = "home";
+            this.home.Size = new System.Drawing.Size(49, 22);
+            this.home.Text = "Домой";
+            this.home.Click += new System.EventHandler(this.home_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // start
+            // 
+            this.start.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
+            this.start.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(26, 22);
+            this.start.Text = "Go";
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // adress
+            // 
+            this.adress.Items.AddRange(new object[] {
+            "yandex.ru",
+            "google.ru",
+            "youtube.ru"});
+            this.adress.Name = "adress";
+            this.adress.Size = new System.Drawing.Size(500, 25);
+            // 
+            // zakladki
+            // 
+            this.zakladki.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.zakladki.Image = ((System.Drawing.Image)(resources.GetObject("zakladki.Image")));
+            this.zakladki.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.zakladki.Name = "zakladki";
+            this.zakladki.Size = new System.Drawing.Size(62, 22);
+            this.zakladki.Text = "Закладки";
+            this.zakladki.Click += new System.EventHandler(this.zakladki_Click);
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 49);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1016, 392);
+            this.browser.TabIndex = 3;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browser_Navigated);
+            this.browser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.browser_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1016, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(35, 17);
+            this.toolStripStatusLabel1.Text = "Done";
+            this.toolStripStatusLabel1.Visible = false;
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.основноеМенюToolStripMenuItem,
+            this.правкаToolStripMenuItem,
+            this.видToolStripMenuItem,
+            this.закладкиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1016, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // основноеМенюToolStripMenuItem
+            // 
+            this.основноеМенюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.назадToolStripMenuItem});
+            this.основноеМенюToolStripMenuItem.Name = "основноеМенюToolStripMenuItem";
+            this.основноеМенюToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.основноеМенюToolStripMenuItem.Text = "Файл";
+            // 
+            // назадToolStripMenuItem
+            // 
+            this.назадToolStripMenuItem.Name = "назадToolStripMenuItem";
+            this.назадToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.назадToolStripMenuItem.Text = "Назад";
+            this.назадToolStripMenuItem.Click += new System.EventHandler(this.назадToolStripMenuItem_Click);
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // закладкиToolStripMenuItem
+            // 
+            this.закладкиToolStripMenuItem.Name = "закладкиToolStripMenuItem";
+            this.закладкиToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.закладкиToolStripMenuItem.Text = "Закладки";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1016, 370);
+            this.tabControl1.TabIndex = 4;
+            this.tabControl1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 441);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Браузер Rozego";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.adress_KeyDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Back;
+        private System.Windows.Forms.ToolStripButton forward;
+        private System.Windows.Forms.ToolStripButton refresh;
+        private System.Windows.Forms.ToolStripButton home;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton start;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.ToolStripButton stop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripButton zakladki;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem основноеМенюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закладкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem назадToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripComboBox adress;
+    }
+}
+
---
+++ rozego/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace rozego
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+           browser.GoBack();
+            
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            browser.Refresh();
+        }
+
+        private void adress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            adress.Text = browser.Url.ToString();
+            toolStripStatusLabel1.Visible = true;        
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            browser.Navigate("http://" + adress.Text);
+            adress.AutoCompleteCustomSource.Add(adress.Text);
+            browser.Navigate(adress.Text);
+            if (!adress.Items.Contains(adress.Text))
+            {
+                adress.Items.Add(adress.Text);
+            }
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            browser.Navigate("http://youtube.ru");
+        }
+
+        private void adress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                browser.Navigate(adress.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            browser.Stop();
+        }
+
+        private void adress_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Maximum = (int)e.MaximumProgress;
+            toolStripProgressBar1.Value = ((int)e.CurrentProgress < 0 || (int)e.MaximumProgress < (int)e.CurrentProgress) ? (int)e.MaximumProgress : (int)e.CurrentProgress;
+        }
+
+        private void browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            toolStripStatusLabel1.Visible = false;
+        }
+
+        private void zakladki_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+           
+            }
+        }
+
+    }
+
+
